b97a423d9f1ea0ef7c06a26037a3905f5220701e
violet <violet91410@gmail.com>
2022-09-27 00:37:13 +0900

    Add apply to library(jwt-decode)

--- src/http/interceptors.js
+++ src/http/interceptors.js
@@ -1,5 +1,6 @@
 // import axios from 'axios'
 import router from '../router/'
+import decode from 'jwt-decode'
 
 /**
  * api 호출 전/후로 요청과 응답을 가로채 특정 작업을 할 수 있다
@@ -9,10 +10,20 @@ export function setInterceptors(createdAxios) {
   createdAxios.interceptors.request.use(
     function (config) {
       // 요청이 전달되기 전에 작업 수행
-      // contain jwtToken and refreshToken at header whenver requesting APIs
+      // contain jwtToken and refreshToken at header whenever requesting APIs
       const jwtToken = localStorage.getItem('jwtToken')
       // const refreshToken = localStorage.getItem('refreshToken')
-      config.headers.Authrization = jwtToken
+      // 현재 토큰 유무 확인 후 헤더에 실어보내기
+      // 없으면 다시 로그인하기??
+      if (jwtToken) {
+        config.headers.Authrization = `Bearer` + jwtToken
+      } else {
+        router.push({ name: 'Login' })
+      }
+      //토큰 정보를 복호화해서 lnbData, roles, userName 변수로 UI제어
+      const decoded = decode(jwtToken)
+      console.error(decoded)
+
       console.log(config)
       return config
     },
