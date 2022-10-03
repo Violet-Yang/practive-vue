4e198e51ccc2d90818a897e6264b6d46fd24fcc7
violet <violet91410@gmail.com>
2022-10-04 00:44:37 +0900

    Update set tab component

---
+++ src/views/Tab.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="h-100">
+    <p>{{ state.tabInfo.title }}</p>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+
+const state = reactive({
+  tabInfo: {
+    title: '입고관리',
+  },
+})
+
+// const props = defineProps({
+//   tabInfo: {
+//     type: Object,
+//     default: () => {
+//       return {}
+//     },
+//   },
+// })
+</script>
+
+<style scoped></style>
---
+++ src/views/shipping/packing/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>여기는 딜리버리</div>
+</template>
+
+<script></script>
+
+<style scoped></style>
